0d86224afdbbda190e72e3ad1da9434467f966ce
Ravindra Singh Dabi <raghav.dabi.7@gmail.com>
2026-01-20 23:28:21 +0530

    add helmet,cors, express.json middlewares

--- app.js
+++ app.js
@@ -1,5 +1,20 @@
 const express = require("express");
+const helmet = require("helmet");
+const cors = require("cors");
+const morgan = require("morgan");
+
 const app = express();
+
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+} else {
+  app.use(morgan("combined"));
+}
+
+
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
 
 app.get("/health", (req, res) => {
   res.status(200).json({ status: "OK" });
