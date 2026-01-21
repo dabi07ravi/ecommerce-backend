fe6269eb6ffae2b6f87d525eec6a875b14c21fbe
Ravindra Singh Dabi <raghav.dabi.7@gmail.com>
2026-01-21 23:33:17 +0530

    auth folder structure + jwt utils

---
+++ src/utils/jwt.js
@@ -0,0 +1,28 @@
+
+
+
+const jwt = require("jsonwebtoken");
+
+
+
+const generateAccessToken = (payload) => {
+  return jwt.sign(payload, process.env.JWT_ACCESS_SECRET, {
+    expiresIn: process.env.JWT_ACCESS_EXPIRES_IN || "15m",
+  });
+};
+
+const generateRefreshToken = (payload) => {
+  return jwt.sign(payload, process.env.JWT_REFRESH_SECRET, {
+    expiresIn: process.env.JWT_REFRESH_EXPIRES_IN || "7d",
+  });
+};
+
+const verifyToken = (token, secret) => {
+  return jwt.verify(token, secret);
+};
+
+module.exports = {
+  generateAccessToken,
+  generateRefreshToken,
+  verifyToken,
+};
